162f84937ecd73a972101a464d401feee73b299f
Michal Dominiczak <michal-dominiczak@o2.pl>
2021-03-31 22:45:33 +0200

    Hilt added

--- app/src/main/java/com/mancode/easycrm/EasyCrmApp.kt
+++ app/src/main/java/com/mancode/easycrm/EasyCrmApp.kt
@@ -2,8 +2,10 @@ package com.mancode.easycrm
 
 import android.app.Application
 import com.jakewharton.threetenabp.AndroidThreeTen
+import dagger.hilt.android.HiltAndroidApp
 
-class App : Application() {
+@HiltAndroidApp
+class EasyCrmApp : Application() {
     override fun onCreate() {
         super.onCreate()
         AndroidThreeTen.init(this)
