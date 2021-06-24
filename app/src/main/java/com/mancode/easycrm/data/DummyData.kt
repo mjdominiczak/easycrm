b6e6592a117d85063db8ca1bb29832875daebca8
Michal Dominiczak <michal-dominiczak@o2.pl>
2021-06-24 23:58:50 +0200

    Release update

--- app/src/main/java/com/mancode/easycrm/data/DummyData.kt
+++ app/src/main/java/com/mancode/easycrm/data/DummyData.kt
@@ -23,8 +23,8 @@ val addresses = listOf(
 )
 
 val contacts = listOf(
-    Contact("1", 1, "Andrzej Witczak", "500123456"),
-    Contact("2", 1, "Kazik", "500456789"),
-    Contact("3", 1, "Dominik Popielski", "600456789", "polswat@polswat.pl"),
-    Contact("4", 2, "Ignacy", "600456789", "ignacy@kan-therm.pl")
+    Contact("1", 1, "Andrzej Witczak", "500123456", hasPhoneNumber = true),
+    Contact("2", 1, "Kazik", "500456789", hasPhoneNumber = true),
+    Contact("3", 1, "Dominik Popielski", "600456789", "polswat@polswat.pl", hasPhoneNumber = true),
+    Contact("4", 2, "Ignacy", "600456789", "ignacy@kan-therm.pl", hasPhoneNumber = true)
 )
